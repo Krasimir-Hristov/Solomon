9210571723f9b261b9e73c851cb01c9a2eb473a0
krasi <krasimir.xristov@gmail.com>
2023-06-14 13:20:48 +0200

    Create my main menu and side-bar and style it

--- solomon/src/App.js
+++ solomon/src/App.js
@@ -1,6 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+
 const App = () => {
   return (
     <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history">
+          <li>Some answare here.....</li>
+        </ul>
+        <nav>
+          <p>Made by Krasimir Hristov</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>SOLOMON</h1>
+        <ul className='feed'>
+
+        </ul>
+        <div className='bottom-section'>
+          <div className='input-container'>
+            <input />
+            <div id='submit'>
+              <FontAwesomeIcon icon={faShare} />
+            </div>
+            <p className='info'>
+              Meet Solomon, the chatbot designed to streamline your communication. With cutting-edge AI technology, Solomon is always ready to assist, inform, and engage in meaningful conversations.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
